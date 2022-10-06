1f73f3ce0c5faf8feef01059a39529adb4bfc61f
Leonardo Silva <leonardorc.geo@gmail.com>
2022-10-05 21:43:49 -0300

    Requisito 09. Criando a classe Monstro

--- src/Character.ts
+++ src/Character.ts
@@ -1,7 +1,7 @@
 import Race, { Elf } from './Races';
 import Archetypes, { Mage } from './Archetypes';
 import Energy from './Energy';
-import Fighter from './Fighter';
+import Fighter, { SimpleFighter } from './Fighter';
 import getRandomInt from './utils';
 
 export default class Character implements Fighter {
@@ -53,7 +53,7 @@ export default class Character implements Fighter {
     return this._lifePoints;
   }
 
-  attack(enemy: Fighter):void {
+  attack(enemy: Fighter | SimpleFighter):void {
     enemy.receiveDamage(this._strength);
   }
 
